c14f0fd061409bf26420ba200f0c0f82eef34375
katherinethai <kthai1618@gmail.com>
2016-05-07 17:23:00 -0400

    fixed bug

--- lib/cli.rb
+++ lib/cli.rb
@@ -10,21 +10,21 @@ class CLI
   end
 
   def run
-    @active = true
-    while @active
-      menu 
-    end
+    @page_num = 1
+    make_articles
+    display_articles
+    menu
   end 
 
   def make_articles(page_url = "https://longreads.com/picks")
-    Article.all.clear
     articles_array = Scraper.scrape_index_page(page_url)
     Article.create_from_collection(articles_array)
+    articles_array.uniq!
   end
 
-  def display_articles
-    Article.all.each_with_index do |article, index|
-    puts "#{index + 1}. #{article.title}\n".colorize(:blue)
+  def display_articles(from_number = 1)
+    Article.all[from_number-1, 10].each.with_index(from_number) do |article, index| 
+    puts "#{index}. #{article.title}\n".colorize(:blue)
     puts "#{article.description}"
     puts "----------------------".colorize(:green)
     end
@@ -42,25 +42,49 @@ class CLI
     puts ""
     puts "  Link:".colorize(:light_blue) + " #{Article.all[index].url}"
     puts "----------------------".colorize(:green)
-    menu
   end
 
   def menu
-    puts "Type \"Search\" to select a page of articles, the number of an article to view its details, or \"exit\" to leave the CLI."
+    display_instructions(@page_num)
     input = gets.chomp
-    if input.downcase == "search"
-      puts "Please enter the page number you would like to view."
-      page_num = gets.chomp.to_i
-      make_articles(page_url(page_num))
-      display_articles
-    elsif (1..10).include?(input.to_i)
+    if input.downcase == "next" 
+      @page_num += 1
+      make_articles(page_url(@page_num))
+      display_articles((@page_num*10)-9)
+      display_instructions(@page_num)
+    elsif input.downcase == "back" && @page_num != 1
+      @page_num -= 1
+      display_articles((@page_num*10)-9)
+      display_instructions(@page_num)
+    elsif input.downcase == "back" && @page_num == 1
+      puts "Sorry! You're already on the first page of articles.".colorize(:red)
+      puts ""
+      display_instructions(@page_num)
+    elsif (1..1000).include?(input.to_i)
       display_article_details(input.to_i - 1)
+      puts "Would you like to view another article? Enter Y or N."
+      input = gets.chomp.downcase
+      if input == "y"
+        run
+      else
+        quit
+      end
     elsif input == "exit"
-      puts "Thank you for reading today!"
-      @active = false
+      quit
     else
       menu
     end
+  end
+
+  def quit
+    puts ""
+    puts "Thank you for reading today!"
+    exit
+  end
+
+  def display_instructions(page_num)
+    puts "You are on page #{page_num}. Type 'next' or 'back' to navigate between pages. Type the number of an article to view its details."
+    puts ""
   end
 
   def page_url(page_num)
